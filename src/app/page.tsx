613ed362b9f5e70211959dd1522d1a01551adc41
huzaifaghazali <huzaifaghazali@gmail.com>
2024-09-23 21:50:08 +0500

    Add github authentication functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+'use client';
 
-import AuthScreen from "@/features/auth/components/auth-screen";
-
+import { Button } from '@/components/ui/button';
+import { useAuthActions } from '@convex-dev/auth/react';
 
 export default function Home() {
- return (
-   <AuthScreen />
- )
+  const { signOut } = useAuthActions();
+
+  return (
+    <div>
+      Logged In
+      <Button onClick={() => signOut()}>Sign Out</Button>
+    </div>
+  );
 }
