83296324edf30543cac6bc6213dcef871418cf43
huzaifaghazali <huzaifaghazali@gmail.com>
2024-09-26 20:59:05 +0500

    Add user button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import { useAuthActions } from '@convex-dev/auth/react';
-import { useRouter } from 'next/navigation';
+import { UserButton } from '@/features/auth/components/user-button';
 
 export default function Home() {
-  const { signOut } = useAuthActions();
-  const router = useRouter();
-
-  function handleLogout() {
-    signOut().finally(() => {
-      router.refresh();
-    });
-  }
-
   return (
     <div>
-      Logged In
-      <Button onClick={() => handleLogout()}>Sign Out</Button>
+      <UserButton />
     </div>
   );
 }
