e0096244905989f7abd479c1024f1cb464bec000
huzaifaghazali <huzaifaghazali@gmail.com>
2024-09-30 14:34:57 +0500

    Add toolbar component

---
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -0,0 +1,15 @@
+'use client'; 
+
+import { Toolbar } from "./toolbar";
+
+interface WorkspaceIdLayoutProps {
+  children: React.ReactNode;
+}
+
+const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+  return <div className='h-full'>
+   <Toolbar />
+   {children}</div>;
+};
+
+export default WorkspaceIdLayout;
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { useWorkspaceId } from '@/hooks/use-workspace-id';
+import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
 
 const WorkspaceIdPage = () => {
   const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
 
-  return <div>Workspace {workspaceId}</div>;
+  return <div>Data:  {JSON.stringify(data)}</div>;
 };
 
 export default WorkspaceIdPage;
