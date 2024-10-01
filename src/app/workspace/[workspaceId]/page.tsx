34f37eabe8d048ac3085bf763a90cb441ce8a282
huzaifaghazali <huzaifaghazali@gmail.com>
2024-10-01 14:52:03 +0500

    Add complete sidebar component

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,13 +1,9 @@
-'use client';
 
-import { useWorkspaceId } from '@/hooks/use-workspace-id';
-import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
 
 const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
 
-  return <div>Data:  {JSON.stringify(data)}</div>;
+
+  return <div> workspace id page</div>;
 };
 
 export default WorkspaceIdPage;
