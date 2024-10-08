d3bf57f1ba78853fcb2cd7ff50c7337ed9516782
huzaifaghazali <huzaifaghazali@gmail.com>
2024-10-08 21:11:33 +0500

    Add jotai intialization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Modals } from '@/components/modals';
 import { Toaster } from '@/components/ui/sonner';
 
 import './globals.css';
+import { JotaiProvider } from '@/components/jotai-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,9 +27,11 @@ export default function RootLayout({
       <html lang='en'>
         <body className={inter.className}>
           <ConvexClientProvider>
-            <Toaster />
-            <Modals />
-            {children}
+            <JotaiProvider>
+              <Toaster />
+              <Modals />
+              {children}
+            </JotaiProvider>
           </ConvexClientProvider>
         </body>
       </html>
