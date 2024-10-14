c0ec34917b5407aa2050714f69e89d0df3355b3e
huzaifaghazali <huzaifaghazali@gmail.com>
2024-10-14 21:52:22 +0500

    Add update workspace api

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -23,7 +23,6 @@ export const create = mutation({
       throw new Error('Unauthorized');
     }
 
-
     const joinCode = generateCode();
 
     const workspaceId = await ctx.db.insert('workspaces', {
@@ -93,5 +92,36 @@ export const getById = query({
     }
 
     return await ctx.db.get(args.id);
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id('workspaces'),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', args.id).eq('userId', userId)
+      )
+      .unique();
+
+    if (!member || member.role !== 'admin') {
+      throw new Error('Unauthorized');
+    }
+
+    await ctx.db.patch(args.id, {
+      name: args.name,
+    });
+
+    return args.id;
   },
 });
