f2c02a4ea7152379074787ddd8d027e3adc21fdb
huzaifaghazali <huzaifaghazali@gmail.com>
2024-10-30 19:11:20 +0500

    Add editor component

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,13 +1,37 @@
-import Editor from '@/components/editor';
+import dynamic from 'next/dynamic';
+import { useRef, useState } from 'react';
+import Quill from 'quill';
+import { toast } from 'sonner';
+
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
+import { useChannelId } from '@/hooks/use-channel-id';
+
+const Editor = dynamic(() => import('@/components/editor'), { ssr: false });
 
 interface ChatInputProps {
   placeholder: string;
 }
 
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
+  const [editorKey, setEditorKey] = useState(0);
+  const [isPending, setIsPending] = useState(false);
+
+  const editorRef = useRef<Quill | null>(null);
+
+  const channelId = useChannelId();
+  const workspaceId = useWorkspaceId();
+
+  const handleSubmit = async () => {};
+
   return (
     <div className='w-full px-5'>
-      <Editor placeholder={placeholder} />
+      <Editor
+        key={editorKey}
+        placeholder={placeholder}
+        disabled={isPending}
+        onSubmit={handleSubmit}
+        innerRef={editorRef}
+      />
     </div>
   );
 };
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -12,28 +12,55 @@ import {
   useState,
 } from 'react';
 
+import { Hint } from './hint';
+
 import { cn } from '@/lib/utils';
 
 import 'quill/dist/quill.snow.css';
+import { Button } from './ui/button';
+
+type EditorValue = {
+  image: File | null;
+  body: string;
+};
 
 interface EditorProps {
-  // disabled?: boolean;
-  // onCancel?: () => void;
+  disabled?: boolean;
   placeholder?: string;
-  // defaultValue?: Delta | Op[];
-  // variant?: "create" | "update";
-  // innerRef?: MutableRefObject<Quill | null>;
-  // onSubmit: ({ image, body }: EditorValue) => void;
+  defaultValue?: Delta | Op[];
+  variant?: 'create' | 'update';
+  innerRef?: MutableRefObject<Quill | null>;
+  onCancel?: () => void;
+  onSubmit: ({ image, body }: EditorValue) => void;
 }
 
-const Editor = ({ placeholder }: EditorProps) => {
+const Editor = ({
+  disabled = false,
+  placeholder = 'Write something...',
+  defaultValue = [],
+  variant = 'create',
+  innerRef,
+  onCancel,
+  onSubmit,
+}: EditorProps) => {
+  const [text, setText] = useState('');
+  const [image, setImage] = useState<File | null>(null);
+  const [isToolbarVisible, setIsToolbarVisible] = useState(true);
+
   const containerRef = useRef<HTMLDivElement>(null);
-  // const submitRef = useRef(onSubmit);
-  //   const disabledRef = useRef(disabled);
+  const submitRef = useRef(onSubmit);
+  const disabledRef = useRef(disabled);
   const placeholderRef = useRef(placeholder);
-  //   const quillRef = useRef<Quill | null>(null);
-  //   const defaultValueRef = useRef(defaultValue);
+  const quillRef = useRef<Quill | null>(null);
+  const defaultValueRef = useRef(defaultValue);
   const imageElementRef = useRef<HTMLInputElement>(null);
+
+  useLayoutEffect(() => {
+    submitRef.current = onSubmit;
+    disabledRef.current = disabled;
+    placeholderRef.current = placeholder;
+    defaultValueRef.current = defaultValue;
+  });
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -46,55 +73,90 @@ const Editor = ({ placeholder }: EditorProps) => {
     const options: QuillOptions = {
       theme: 'snow',
       placeholder: placeholderRef.current,
-      // modules: {
-      //   toolbar: [
-      //     ['bold', 'italic', 'strike'],
-      //     ['link'],
-      //     [{ list: 'ordered' }, { list: 'bullet' }],
-      //   ],
-      //   keyboard: {
-      //     bindings: {
-      //       enter: {
-      //         key: 'Enter',
-      //         handler: () => {
-      //           const text = quill.getText();
-      //           const addedImage = imageElementRef.current?.files?.[0] || null;
+      modules: {
+        toolbar: [
+          ['bold', 'italic', 'strike'],
+          ['link'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+        ],
+        keyboard: {
+          bindings: {
+            enter: {
+              key: 'Enter',
+              handler: () => {
+                const text = quill.getText();
+                const addedImage = imageElementRef.current?.files?.[0] || null;
 
-      //           const isEmpty =
-      //             !addedImage &&
-      //             text.replace(/<(.|\n)*?>/g, '').trim().length === 0;
+                const isEmpty =
+                  !addedImage &&
+                  text.replace(/<(.|\n)*?>/g, '').trim().length === 0;
 
-      //           if (isEmpty) return;
+                if (isEmpty) return;
 
-      //           const body = JSON.stringify(quill.getContents());
-      //           //  submitRef.current?.({
-      //           //    body,
-      //           //    image: addedImage,
-      //           //  });
-      //         },
-      //       },
-      //       shift_enter: {
-      //         key: 'Enter',
-      //         shiftKey: true,
-      //         handler: () => {
-      //           quill.insertText(quill.getSelection()?.index || 0, '\n');
-      //         },
-      //       },
-      //     },
-      //   },
-      // },
+                const body = JSON.stringify(quill.getContents());
+                submitRef.current?.({
+                  body,
+                  image: addedImage,
+                });
+              },
+            },
+            shift_enter: {
+              key: 'Enter',
+              shiftKey: true,
+              handler: () => {
+                quill.insertText(quill.getSelection()?.index || 0, '\n');
+              },
+            },
+          },
+        },
+      },
     };
 
     const quill = new Quill(editorContainer, options);
+
+    quillRef.current = quill;
+    quillRef.current.focus();
+
+    if (innerRef) {
+      innerRef.current = quill;
+    }
+
+    quill.setContents(defaultValueRef.current);
+
+    setText(quill.getText());
+    quill.on(Quill.events.TEXT_CHANGE, () => {
+      setText(quill.getText());
+    });
 
     return () => {
       quill.off(Quill.events.TEXT_CHANGE);
 
       if (container) {
-         container.innerHTML = "";
-       }
+        container.innerHTML = '';
+      }
+
+      if (quillRef.current) {
+        quillRef.current = null;
+      }
+
+      if (innerRef) {
+        innerRef.current = null;
+      }
     };
-  }, []);
+  }, [innerRef]);
+
+  const toggleToolbar = () => {
+    setIsToolbarVisible((current) => !current);
+    const toolbarElement = containerRef.current?.querySelector('.ql-toolbar');
+
+    if (toolbarElement) {
+      toolbarElement.classList.toggle('hidden');
+    }
+  };
+
+  // We consider the editor empty if either:
+  //  the text content of the editor is empty, after removing all HTML tags (we use a regular expression to remove all HTML tags, and then check that the resulting string is empty)
+  const isEmpty = !image && text.replace(/<(.|\n)*?>/g, '').trim().length === 0;
 
   return (
     <div className='flex flex-col'>
@@ -103,8 +165,88 @@ const Editor = ({ placeholder }: EditorProps) => {
           'flex flex-col overflow-hidden rounded-md border border-slate-200 bg-white transition focus-within:border-slate-300 focus-within:shadow-sm'
         )}
       >
-        <div ref={containerRef} />
+        <div ref={containerRef} className='ql-custom h-full' />
+        <div className='z-[5] flex px-2 pb-2'>
+          <Hint label={isToolbarVisible  ? 'Hide formatting' : 'Show formatting'}>
+            <Button
+              disabled={disabled}
+              size='iconSm'
+              variant='ghost'
+              onClick={toggleToolbar}
+            >
+              <PiTextAa className='size-4' />
+            </Button>
+          </Hint>
+
+          <Button disabled={disabled} size='iconSm' variant='ghost'>
+            <Smile className='size-4' />
+          </Button>
+
+          {variant === 'create' && (
+            <Hint label='Image'>
+              <Button
+                disabled={disabled}
+                size='iconSm'
+                variant='ghost'
+                onClick={() => {}}
+              >
+                <ImageIcon className='size-4' />
+              </Button>
+            </Hint>
+          )}
+
+          {variant === 'update' && (
+            <div className='ml-auto flex items-center gap-x-2'>
+              <Button
+                size='sm'
+                disabled={disabled}
+                variant='outline'
+                onClick={onCancel}
+              >
+                Cancel
+              </Button>
+
+              <Button
+                size='sm'
+                disabled={disabled || isEmpty}
+                onClick={() => {}}
+                className='bg-[#007a5a] text-white hover:bg-[#007a5a]/80'
+              >
+                Save
+              </Button>
+            </div>
+          )}
+
+          {variant === 'create' && (
+            <Button
+              size='iconSm'
+              disabled={disabled || isEmpty}
+              onClick={() => {}}
+              className={cn(
+                'ml-auto',
+                isEmpty
+                  ? 'bg-white text-muted-foreground hover:bg-white'
+                  : 'bg-[#007a5a] text-white hover:bg-[#007a5a]/80'
+              )}
+            >
+              <MdSend className='size-4' />
+            </Button>
+          )}
+        </div>
       </div>
+
+      {variant === 'create' && (
+        <div
+          className={cn(
+            'flex justify-end p-2 text-[10px] text-muted-foreground opacity-0 transition',
+            !isEmpty && 'opacity-100'
+          )}
+        >
+          <p>
+            <strong>Shift + Return</strong> to add a new line
+          </p>
+        </div>
+      )}
     </div>
   );
 };
